68caa63d702ea4926a8c8c30a85d6d2a15b9f1ec
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-09-16 15:00:00 +0200

    Switch order of imports

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,13 +4,14 @@ import 'angular-route';
 import { appConfig } from './app.config';
 import { MoviesService } from './shared/movies.service';
 import { MoviePanelComponent } from './shared/movie-panel/movie-panel.component';
+import { MovieSearchBoxComponent } from './shared/movie-search-box/movie-search-box.component.js';
+import { MoviesMenuComponent } from './shared/movies-menu/movies-menu.component.js';
 import { MoviesSearchComponent } from './movies-search/movies-search.component';
 import { MoviesDetailsComponent } from './movies-details/movies-details.component';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 import { MoviesAddComponent } from './movies-add/movies-add.component';
 import { MovieNotFoundErrorComponent } from './movie-error/movie-error.component';
-import { MovieSearchBoxComponent } from './shared/movie-search-box/movie-search-box.component.js';
-import { MoviesMenuComponent } from './shared/movies-menu/movies-menu.component.js';
+
 
 var appModule = angular.module('moviesApp', ['ngRoute'])
        .config(appConfig)
